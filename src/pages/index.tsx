4314cd992122e98fe84e1144a3e6f396b187dcba
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-09-30 17:49:04 +0800

    Route topic cards to the rankings page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,8 +38,13 @@ type TopicCardProps = {
 };
 
 const TopicCard = ({ id, title, date }: TopicCardProps) => {
+  const router = useRouter();
+
   return (
-    <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
+    <section
+      onClick={() => router.push(`/topic/${id}/rankings`)}
+      className="flex cursor-pointer flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105"
+    >
       <h2 className="text-lg text-gray-700">{title}</h2>
       <p className="text-sm text-gray-600">{date}</p>
     </section>
